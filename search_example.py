c4613ff321e0352adad46d46dca9398be5caaf16
dineshjagadeesan <jdinsh@gmail.com>
2022-05-12 13:23:52 -0400

    Added changes

--- search_example.py
+++ search_example.py
@@ -1,4 +1,6 @@
 from commons.google_search_wrapper import do_search_only10
 
-items = do_search_only10('EXECUTEAM CORPORATION CEO')
-print (items[0])
+items = do_search_only10('EXECUTEAM CORPORATION HOUSTON TX 77057')
+
+
+print(items[0])
